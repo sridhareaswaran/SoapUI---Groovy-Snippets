bd9b36e7a0ef6804462e09ecd7d1e966fccea285
Sridhar Easwaran <sridhar.1788@gmail.com>
2015-12-21 10:22:10 -0500

    Update Get Assertion & their status at test case level.groovy

--- Get Assertion & their status at test case level.groovy
+++ Get Assertion & their status at test case level.groovy
@@ -6,7 +6,7 @@ tcase.getTestStepList().each
 {
   it->
           log.info("==> " + it.getLabel())
-          assertions = it.testStep.getAssertionList().size() - 1
+          assertions = it.getAssertionList().size() - 1
           for(num in 0..assertions)
           {
              log.info("${it.getAssertionAt(num).getStatus()}  on asserting for - ${it.getAssertionAt(num).getName()}")
