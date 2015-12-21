b39074fb962f94361fd3b96848542d6744cb3cd0
Sridhar Easwaran <sridhar.1788@gmail.com>
2015-12-21 10:30:13 -0500

    :rocket:

--- Get Assertion & their status at test case level.groovy
+++ Get Assertion & their status at test case level.groovy
@@ -1,20 +1,24 @@
-import com.eviware.soapui.model.testsuite.Assertable
+/* Get Assertion & their status at test case level.groovy */
+
+import com.eviware.soapui.impl.wsdl.teststeps.assertions.ResponseAssertion
 
 def tcase = testRunner.testCase;
 log.info("Assertion Status for : ${tcase.getLabel()}")
 tcase.getTestStepList().each
 {
   it->
-          log.info("==> " + it.getLabel())
-          assertions = it.getAssertionList().size() - 1
-          for(num in 0..assertions)
-          {
-             log.info("${it.getAssertionAt(num).getStatus()}  on asserting for - ${it.getAssertionAt(num).getName()}")
-             error = it.getAssertionAt(num).getErrors()
-             if (error != null)
-             {
-                 log.info(error[0].getMessage())
-             }
-          }
-            
+  		if(it.config.type.equals("restrequest"))
+  		{
+	          log.info("==> " + it.getLabel())
+	          assertions = it.getAssertionList().size() - 1
+	          for(num in 0..assertions)
+	          {
+	             log.info("${it.getAssertionAt(num).getStatus()}  on asserting for - ${it.getAssertionAt(num).getName()}")
+	             error = it.getAssertionAt(num).getErrors()
+	             if (error != null)
+	             {
+	                 log.info(error[0].getMessage())
+	             }
+	          }
+  		}
 }
