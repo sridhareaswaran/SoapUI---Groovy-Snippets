2d1a351fa9ba6de307ac9107f145942555a0bcde
Sridhar Easwaran <sridhar.1788@gmail.com>
2015-12-28 05:02:34 -0500

    more generic way

--- add header without overwriting existing ones.groovy
+++ add header without overwriting existing ones.groovy
@@ -1,5 +1,5 @@
 import com.eviware.soapui.impl.wsdl.teststeps.*
-import com.eviware.soapui.support.types.StringToStringsMap;
+import com.eviware.soapui.support.types.StringToStringMap;
 
 def project = testRunner.testCase.testSuite.project;
 
@@ -11,17 +11,20 @@ project.getTestSuiteList().each{
                     tstep->
                             if( tstep instanceof WsdlTestRequestStep || tstep instanceof RestTestRequestStep) {
                                  def new_headers = new StringToStringMap()
-                                 def headers = it.getHttpRequest().getRequestHeaders()
+                                 def headers = tstep.getHttpRequest().getRequestHeaders()
                                  headers.each
                                  {
-                                  	h_name,h_value ->
+                                 	def h_name = it.key
+                                 	def h_value = it.value[0]
                                   	new_headers.put(h_name,h_value)
                                  }
                                  //here goes your new header(s)
                                  new_headers.put("header_name1","value");
                                  new_headers.put("header_name2","value");
-                                 it.getHttpRequest().setRequestHeaders(new_headers)
+                                 tstep.getHttpRequest().setRequestHeaders(new_headers)
                             }
                 }
         }
 }
+
+log.info "done ;)"
